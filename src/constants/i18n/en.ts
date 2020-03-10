5f9c5e7fbadf0e55715202f0f23301c30dad6d05
Anton Karytka <antnkrtk@gmail.com>
2020-03-10 16:48:55 +0300

    Convert rest of simulated day properties to translation format.

--- src/constants/i18n/en.ts
+++ src/constants/i18n/en.ts
@@ -1,9 +1,19 @@
-import {
-  GROWTH_FACTOR
-} from '../simulatedDayProperties';
+import * as SIMULATED_DAY_PROPERTY from '../simulatedDayProperties';
 
 export default {
-  [GROWTH_FACTOR]: {
+  [SIMULATED_DAY_PROPERTY.ACTIVE_CASES]: {
+    name: 'Active Cases',
+  },
+  [SIMULATED_DAY_PROPERTY.GROWTH_FACTOR]: {
     name: 'Growth Factor',
+  },
+  [SIMULATED_DAY_PROPERTY.NEW_CASES]: {
+    name: 'New Cases',
+  },
+  [SIMULATED_DAY_PROPERTY.TOTAL_CASES]: {
+    name: 'Total Cases',
+  },
+  [SIMULATED_DAY_PROPERTY.TOTAL_FATALITIES]: {
+    name: 'Total Fatalities',
   }
 };
