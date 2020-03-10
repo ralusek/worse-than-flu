11a20cb2ee3ee9c662c4cee3152f030b86adef18
Anton Karytka <antnkrtk@gmail.com>
2020-03-10 17:22:12 +0300

    Add Russian translations.

--- src/constants/i18n/index.ts
+++ src/constants/i18n/index.ts
@@ -2,11 +2,12 @@ import { Language } from '../index';
 import { Translation } from './types';
 
 import en from './translations/en';
+import ru from './translations/ru';
 
 type TranslationByLanguage = { [language in Language]: Translation};
 const TRANSLATION_BY_LANGUAGE: TranslationByLanguage = {
   [Language.English]: en,
-  [Language.Russian]: en,
+  [Language.Russian]: ru,
 };
 
 export default TRANSLATION_BY_LANGUAGE;
---
+++ src/constants/i18n/translations/ru.ts
@@ -0,0 +1,33 @@
+import * as CONTROL_LABEL from '../../controlLabels';
+import * as SIMULATED_DAY_PROPERTY from '../../simulatedDayProperties';
+import * as TITLE from '../../titles';
+
+export default {
+  [TITLE.ROOT_VIEW]: {
+    name: 'Это не простуда, приятель.',
+  },
+  [CONTROL_LABEL.CHANCE_OF_SPREADING]: {
+    name: 'Шанс Распространения (1 к)',
+  },
+  [CONTROL_LABEL.DAYS_TO_SIMULATE]: {
+    name: 'Дней Симуляции',
+  },
+  [CONTROL_LABEL.NUMBER_OF_DAILY_PERSONAL_INTERACTIONS]: {
+    name: 'Количество Ежедневных Персональных Контактов',
+  },
+  [SIMULATED_DAY_PROPERTY.ACTIVE_CASES]: {
+    name: 'Активные Случаи',
+  },
+  [SIMULATED_DAY_PROPERTY.GROWTH_FACTOR]: {
+    name: 'Фактор Роста',
+  },
+  [SIMULATED_DAY_PROPERTY.NEW_CASES]: {
+    name: 'Новые Случаи',
+  },
+  [SIMULATED_DAY_PROPERTY.TOTAL_CASES]: {
+    name: 'Всего Случаев',
+  },
+  [SIMULATED_DAY_PROPERTY.TOTAL_FATALITIES]: {
+    name: 'Всего Смертей',
+  }
+};
