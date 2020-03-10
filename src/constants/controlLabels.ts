bc21acfdf11c8a50d3f2088d9befc2a2ecbbb9fb
Anton Karytka <antnkrtk@gmail.com>
2020-03-10 16:55:54 +0300

    Prepare control labels and titles for translation.

---
+++ src/constants/controlLabels.ts
@@ -0,0 +1,3 @@
+export const CHANCE_OF_SPREADING = 'chanceOfSpreading';
+export const DAYS_TO_SIMULATE = 'daysToSimulate';
+export const NUMBER_OF_DAILY_PERSONAL_INTERACTIONS = 'numberOfDailyPersonalInteractions';
